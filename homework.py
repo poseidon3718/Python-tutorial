26bd1772a2d86be293f77f6a3a200b428c584a81
poseidon3718 <85330911+poseidon3718@users.noreply.github.com>
2024-03-28 15:20:06 +0900

    Update homework.py

--- homework.py
+++ homework.py
@@ -2,7 +2,10 @@
 
 #구구단 과제
 
-for x in range(2, 10):
-    for y in range(2, 10): 
+for x in range(1, 10):
+    print()
+    print(f"{x} Times Table")
+    print()
+    for y in range(1, 10): 
         print(f"{x} * {y} = {x*y}") 
  
